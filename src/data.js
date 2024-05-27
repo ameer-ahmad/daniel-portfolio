b0ecb4ec4efb5bf5e932aca5abfefaaa6cdb5ca9
ameer-ahmad <ahmaamee@sheridancollege.ca>
2024-05-27 11:40:53 -0400

    update

--- src/data.js
+++ src/data.js
@@ -22,12 +22,99 @@ import logoBlack from './images/logo-black.png';
 import drawing from './images/drawing.png';
 import jersey from './images/jersey.png';
 import poster1 from './images/poster1.jpg';
+import book1 from './images/book1.png';
+import book2 from './images/book2.png';
+import book3 from './images/book3.png';
+import book4 from './images/book4.png';
+import book5 from './images/book5.png';
+import book6 from './images/book6.png';
+import book7 from './images/book7.png';
+import book8 from './images/book8.png';
+import book9 from './images/book9.png';
+import book10 from './images/book10.png';
+import book11 from './images/book11.png';
+import book12 from './images/book12.png';
+import book13 from './images/book13.png';
+import book14 from './images/book14.png';
+import book15 from './images/book15.png';
+import book16 from './images/book16.png';
 
 const data = [
   {
+    id: 0,
+    title: 'Analog » Digital, Undergraduate Book 2024',
+    image: bookCovers,
+    subPages: [
+      {
+        image: book1,
+        caption: '',
+      },
+      {
+        image: book2,
+        caption: '',
+      },
+      {
+        image: book3,
+        caption: '',
+      },
+      {
+        image: book4,
+        caption: '',
+      },
+      {
+        image: book5,
+        caption: '',
+      },
+      {
+        image: book6,
+        caption: '',
+      },
+      {
+        image: book7,
+        caption: '',
+      },
+      {
+        image: book8,
+        caption: '',
+      },
+      {
+        image: book9,
+        caption: '',
+      },
+      {
+        image: book10,
+        caption: '',
+      },
+      {
+        image: book11,
+        caption: '',
+      },
+      {
+        image: book12,
+        caption: '',
+      },
+      {
+        image: book13,
+        caption: '',
+      },
+      {
+        image: book14,
+        caption: '',
+      },
+      {
+        image: book15,
+        caption: '',
+      },
+      {
+        image: book16,
+        caption: '',
+      },
+    ],
+  },
+  {
     id: 1,
     title: 'Analog » Digital, Undergraduate Thesis 2023-24',
-    image: bookCovers,
+    image: scroll,
     subPages: [
       {
         image: letters,
